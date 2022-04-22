2ddd1311ba86642fcd3dab6975da601020d641e3
unknown <kutetsky93@gmail.com>
2022-04-22 15:00:02 +0300

    slider section

--- asset/js/app.js
+++ asset/js/app.js
@@ -1,34 +1,28 @@
 "use strict";
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+$(document).ready(function () {
+  $('#calcTotal1, #calcTotal2').hide();
+  $('#calcBtn').click(function () {
+    event.preventDefault();
+    var weight = $('#calcWeight').val();
+    var height = $('#calcHeight').val() / 100;
+    var BMI = Math.round(weight / Math.pow(height, 2));
 
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
-
-var tessst = function tessst() {
-  var _console;
-
-  (_console = console).log.apply(_console, arguments);
-};
-
-var abc = /*#__PURE__*/function () {
-  function abc() {
-    _classCallCheck(this, abc);
-
-    return;
-  }
-
-  _createClass(abc, [{
-    key: "testmethod",
-    value: function testmethod() {
-      console.log('testingclass');
+    if (BMI > 25) {
+      $('#calcTotal1, #calcTotal2, #calcTotal3').hide();
+      $('#calcTotal3').show();
     }
-  }]);
 
-  return abc;
-}();
+    if (BMI > 18 && BMI < 25) {
+      $('#calcTotal1, #calcTotal2, #calcTotal3').hide();
+      $('#calcTotal2').show();
+    }
 
-var testobj = new abc();
-testobj.testmethod();
-tessst('d', 'www', 'dsadas', 'dasdssad');
+    if (BMI < 18) {
+      $('#calcTotal1, #calcTotal2, #calcTotal3').hide();
+      $('#calcTotal1').show();
+    }
+
+    BMI = null;
+  });
+});
--- src/js/app.js
+++ src/js/app.js
@@ -1,20 +1,23 @@
-let tessst = (...args) => {
-    console.log(...args)
-}
+$(document).ready(function () {
+  $('#calcTotal1, #calcTotal2').hide()
+  $('#calcBtn').click(function () {
+    event.preventDefault()
 
-class abc {
-    constructor() {
-        return
+    let weight = $('#calcWeight').val()
+    let height = $('#calcHeight').val() / 100
+
+    let BMI = Math.round(weight / Math.pow(height, 2))
+
+    if (BMI > 25) {
+      $('#calcTotal1, #calcTotal2, #calcTotal3').hide()
+      $('#calcTotal3').show()
+    } if (BMI > 18 && BMI < 25) {
+      $('#calcTotal1, #calcTotal2, #calcTotal3').hide()
+      $('#calcTotal2').show()
+    } if (BMI < 18) {
+      $('#calcTotal1, #calcTotal2, #calcTotal3').hide()
+      $('#calcTotal1').show()
     }
-
-    testmethod() {
-        console.log('testingclass')
-    }
-}
-
-const testobj = new abc()
-
-testobj.testmethod()
-
-
-tessst('d', 'www', 'dsadas', 'dasdssad')
+    BMI = null
+  })
+})
